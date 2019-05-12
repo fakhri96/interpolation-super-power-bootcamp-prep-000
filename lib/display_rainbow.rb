7d350a20b73d6d3eb3997f2c4f5d54b14d897fc5
declarative-banner-0824 <faxri96@live.com>
2019-05-12 10:53:58 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,5 +1,7 @@
 
-def display_rainbow(['red', 'organge', 'yellow', 'green', 'blue', 'indigo', 'violet']).
+def display_rainbow(colors)
+  
+  colors = ["red", 'organge', 'yellow', 'green', 'blue', 'indigo', 'violet']
   
   puts "R: #{display_rainbow[0]}, O: #{display_ranibow[1]}, Y: #{display_ranibow[2]}, G: #{display_ranibow[3]}, B: #{display_ranibow[4]}, I: #{display_ranibow[5]}, V: #{display ranibow[6]}"
 end
